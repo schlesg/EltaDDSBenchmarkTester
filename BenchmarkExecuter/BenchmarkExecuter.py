1400df62359b818a0e12fedc407208d8c2e3277c
Gal Schlesinger <schlesgal@gmail.com>
2019-12-26 18:55:04 +0200

    remove python executer

--- BenchmarkExecuter/BenchmarkExecuter.py
+++ BenchmarkExecuter/BenchmarkExecuter.py
@@ -1,19 +1,43 @@
 
 import os
 import subprocess
+import multiprocessing
 
 os.chdir(os.path.dirname(__file__))
 os.path.dirname(os.path.realpath(__file__)) 
 
-publihser = "objs\\x64Win64VS2011\\BenchmarkType_publisher.exe"
-print(publihser)
-#os.system("start /B start" + publihser +"  @cmd /k ")
+import os
+import multiprocessing
+
+NUMBER_OF_SESSIONS = 10
+
+def worker():
+    while True:
+        os.system("dir")
 
 
-#os.system("start /wait cmd /c {command}")
+if __name__ == '__main__':
+    jobs = []
+    for i in range( NUMBER_OF_SESSIONS ):
+        p = multiprocessing.Process(target=worker)
+        jobs.append(p)
+        p.start()
 
-#subprocess.Popen(publihser, shell=True, cwd = os.getcwd())
 
-subprocess.call(["objs\\x64Win64VS2011\\BenchmarkType_publisher.exe"], cwd="D:\GITRepo\EltaDDSBenchmarkTester")
+#publihser = "C:\\Users\\Gal2IB\\Source\\Repos\\schlesg\\EltaDDSBenchmarkTester\\objs\\x64Win64VS2017\\BenchmarkType_publisher.exe"
+#print(publihser)
+##os.system("start /B start" + publihser +"  @cmd /k ")
+
+##os.system(publihser)
+##os.system(publihser)
+
+##os.system("start /wait cmd /c {command}")
+##multiprocessing.process(publihser)
+
+#a = subprocess.Process(publihser, start_new_session=True, cwd = os.getcwd())
+#subprocess.Popen(publihser, start_new_session=True, cwd = os.getcwd())
+
+#a.wait()
+#subprocess.call(["\\objs\\x64Win64VS2011\\BenchmarkType_publisher.exe"], cwd="C:\\Users\\Gal2IB\\Source\\Repos\\schlesg\\EltaDDSBenchmarkTester")
 
 #os.system("start /wait cmd /c {command}")
